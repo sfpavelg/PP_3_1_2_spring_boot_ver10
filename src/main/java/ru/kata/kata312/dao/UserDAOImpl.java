d74e964b974fd84d2411b13845cc3355cd8bdaac
ADMIN <daniltarasov@yandex.ru>
2023-01-06 21:53:12 +0300

    first commit

--- src/main/java/ru/kata/kata312/dao/UserDAOImpl.java
+++ src/main/java/ru/kata/kata312/dao/UserDAOImpl.java
@@ -1,10 +1,8 @@
-
-
-
 package ru.kata.kata312.dao;
 
 import org.springframework.stereotype.Repository;
 import ru.kata.kata312.model.User;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -30,7 +28,6 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getUserById(int id) {
-
         return entityManager.find(User.class, id);
     }
 
@@ -41,7 +38,9 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public void delete(int id) {
-        entityManager.remove(entityManager.find(User.class, id));
+        User user = getUserById(id);
+        if (user != null) {
+            entityManager.remove(user);
+        }
     }
 }
-
